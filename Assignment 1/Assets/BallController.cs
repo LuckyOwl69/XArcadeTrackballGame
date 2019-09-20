fceeb1386597d90395e3b153932acbcf88ceddfd
LuckyOwl69 <cwaites@hotmail.com>
2019-09-20 20:37:23 +1000

    Initial test

---
+++ Assignment 1/Assets/BallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float ballSpeed = 300;
+
+    void Update()
+    {
+        float xAxisSpeed = Input.GetAxis("Mouse X");
+        float yAxisSpeed = Input.GetAxis("Mouse Y");
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddTorque(new Vector3(xAxisSpeed, 0, yAxisSpeed) * ballSpeed * Time.deltaTime);
+    }
+}
---
+++ Assignment 1/Assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Ball;
+    public float xOffset = 5, yOffset = 5, zOffset;
+
+    void Update()
+    {
+        transform.position = Ball.transform.position + new Vector3(xOffset, yOffset, zOffset);
+        transform.LookAt(Ball.transform.position);
+    }
+}
