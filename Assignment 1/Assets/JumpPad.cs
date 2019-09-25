9433224229189046fdeec4e605e799e600e76a75
LuckyOwl69 <cwaites@hotmail.com>
2019-09-25 11:33:21 +1000

    Added a little maze thing

---
+++ Assignment 1/Assets/JumpPad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public int speed;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * speed);
+        }
+    }
+}
