940995d8cce93e912724b5e9a6df9c567911a2ae
LuckyOwl69 <cwaites@hotmail.com>
2019-09-27 22:11:16 +1000

    Added wall that breaks when you touch it

--- Assignment 1/Assets/BallController.cs
+++ Assignment 1/Assets/BallController.cs
@@ -47,8 +47,9 @@ public class BallController : MonoBehaviour
 
         return false;
     }
-        void Update()
-    {
+    void Update()
+        {
+        
         //locks cursor to the game window
         Cursor.lockState = CursorLockMode.Locked;
 
@@ -64,31 +65,10 @@ public class BallController : MonoBehaviour
 
 
 
-            Debug.DrawRay(body.transform.position, Vector3.down, Color.red, 2f);
-        /*if (hit.transform.tag == "Ground")
-         {
+        Debug.DrawRay(body.transform.position, Vector3.down, Color.red, 2f);
+        
+        checkGround = GroundCheck();
 
-
-             Debug.Log("I am on the ground");
-
-             jumpCount = 
-
-
-
-         }
-
-     }
-     else
-     {
-         Debug.Log("STOP JUMPING");
-
-         if (Input.GetKeyDown(KeyCode.Mouse1))
-         {
-             body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-             jumpCount += 1;
-         }
-     }*/
-             checkGround = GroundCheck();
             ////jump function, right mouse button
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
@@ -105,50 +85,16 @@ public class BallController : MonoBehaviour
             }
 
 
-
-            //jump function with ground check
-            /* if (IsGrounded() && Input.GetKeyDown(KeyCode.Mouse1))
-             {
-                 body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-             }*/
-
-
-            //slam function, left mouse button
-            /* if (IsNotGrounded() && Input.GetKeyDown(KeyCode.Mouse0))
-             {
-                 body.AddForce(Vector3.down * 10 * jumpForce, ForceMode.Impulse);
-             }*/
-
-            ////slam function with ground check
-            /*if (IsNotGrounded() && Input.GetKeyDown(KeyCode.Mouse0))
-            {
-               body.AddForce(Vector3.down * 10 * jumpForce, ForceMode.Impulse);
-            }*/
-
-            //move fast function, middle mouse button
-            if (Input.GetKey(KeyCode.Mouse2))
-            {
-                body.AddForce(movement * (ballSpeed + 100));
-            }
+        //slam function, left mouse button
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            body.AddForce(Vector3.down * 10 * jumpForce, ForceMode.Impulse);
         }
 
-        //check to see if player is on the ground
-        /* private bool IsGrounded()
-         {
-             return Physics.CheckCapsule(sphereCollider.bounds.center, 
-                 new Vector3(sphereCollider.bounds.center.x, sphereCollider.bounds.center.y, sphereCollider.bounds.center.z), 
-                 sphereCollider.radius * .9f, ground);
-
-
-         }
-
-         //check to see if player is in the air
-         private bool IsNotGrounded()
-         {
-             return Physics.CheckCapsule(sphereCollider.bounds.center,
-                 new Vector3(sphereCollider.bounds.center.x, sphereCollider.bounds.center.y, sphereCollider.bounds.center.z),
-                 sphereCollider.radius * .9f, allLayers);
-         }
-         */
-    
+        //move fast function, middle mouse button
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            body.AddForce(movement * (ballSpeed + 100));
+        }
+    }
 }
